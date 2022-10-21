14d9a5f5132187609d7561773f59ff4e6f402cc8
sangeon22 <tkddjsdl33@hanmail.net>
2022-10-21 15:50:00 +0900

    POST Mapping

--- mybatisPrj/src/main/java/com/example/demo/CompanyController.java
+++ mybatisPrj/src/main/java/com/example/demo/CompanyController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class CompanyController {
 	@Autowired
 	private CompanyMapper companyMapper;
 	
+	@PostMapping("")
 	public int post(@RequestBody Company company) {
 		return companyMapper.insert(company);
 	}
